1ab53850c17b501d3f73629f1a91f35bad0c6204
darakudou <blackrx.robo.vaio@gmail.com>
2020-08-27 23:51:35 +0900

    wip

--- src/001_spam_restaulant.py
+++ src/001_spam_restaulant.py
@@ -6,6 +6,6 @@ def OrderMenu(menuName):
     return f"spam, {menuName}, spam and spam!"
 
 if __name__ == "__main__":
-    Input  = sys.argv[1]   
+    Input  = sys.argv[1]
     orderdMenu = OrderMenu(Input);
     print(orderdMenu)
